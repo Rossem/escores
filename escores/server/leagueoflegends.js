2d01fb01fab16c718956346be4a08113023f1577
Rossem <rrsbrg@gmail.com>
2015-01-29 21:31:53 -0500

    fetching data from lolesports now working, wildturtle is bad

--- escores/server/leagueoflegends.js
+++ escores/server/leagueoflegends.js
@@ -4,29 +4,32 @@ matches = new Mongo.Collection("leagueoflegendsMatches");
 
 
 function getMatch(id) {
-    var data = Meteor.http.call("GET", "http://na.lolesports.com:80/api/match/" + id.toString() + ".json");
-    var parsedData = JSON.parse(data);
+    var url = "http://na.lolesports.com:80/api/match/" + id.toString() + ".json"
+    var result = Meteor.http.get(url);
+    var parsedData = JSON.parse(result.content);
 
-    if (parsedData['ifFinished'] != '0') {
-        id_count += 1;
-        matches.insert({
-            tournament: parsedData['tournament'],
-            dateTime: parsedData['dateTime'],
-            winnerId: parsedData['winnerId'],
-            matchId: parsedData['matchId'],
-            maxGames: parsedData['maxGames'],
-            blue: parsedData['contestants']['blue'],
-            red: parsedData['contestants']['red'],
-            liveStreams: parsedData['liveStreams'],
-            polldaddyId: parsedData['polldaddyId'],
-            games: parsedData['games'],
-            name: parsedData['name']
-        })
+    if (result.statusCode == 200) {
+        if (parsedData['ifFinished'] != '0') {
+            id_count += 1;
+            matches.insert({
+                tournament: parsedData['tournament'],
+                dateTime: parsedData['dateTime'],
+                winnerId: parsedData['winnerId'],
+                matchId: parsedData['matchId'],
+                maxGames: parsedData['maxGames'],
+                blue: parsedData['contestants']['blue'],
+                red: parsedData['contestants']['red'],
+                liveStreams: parsedData['liveStreams'],
+                polldaddyId: parsedData['polldaddyId'],
+                games: parsedData['games'],
+                name: parsedData['name']
+            })
+        }
     }
 }
 
 
     
 if (Meteor.isServer) {
-   Meteor.setInterval(getMatch(id_count), 60);
+   Meteor.setInterval(function() {getMatch(id_count);}, 60);
 }
