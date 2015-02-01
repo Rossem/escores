22c463929477c908223746e5649a58aba418a414
jameswang14 <james.jwang14@gmail.com>
2015-02-01 01:42:35 -0500

    added routing

---
+++ escores/lib/router.js
@@ -0,0 +1,15 @@
+
+Router.configure({
+  layoutTemplate: 'layout',
+  //loadingTemplate: 'loading',
+  waitOn: function() {Meteor.subscribe('matches')}
+});
+Router.route('/', {name: 'appBody'});
+Router.route('/leaguematches/:matchId',{
+	name: 'leaguematchPage',
+	data: function() { return matches.findOne({matchId: this.matchId});}
+});
+Router.route('/leagueteams/:teamId',{
+	name: 'leagueteamPage',
+	data: function() {}
+});
